0658e490fd85d0432e59926ede891240961ccd8c
impotatook <imapotatook123@gmail.com>
2024-07-23 01:51:20 +0930

    Refactor: bat script to use python venv

--- RunMe.bat
+++ RunMe.bat
@@ -2,60 +2,50 @@
 setlocal enabledelayedexpansion
 
 rem Check if Python is installed
-python --version >nul 2>&1
+py --version >nul 2>&1
 if %errorlevel% neq 0 (
     echo Python is not installed. Please install it and try again.
     echo Download Python by pressing Enter.
     pause >nul
-    start "" "https://apps.microsoft.com/detail/9NRWMJP3717K?hl=fr-ca&gl=US"
+    start "" "https://www.python.org/downloads/"
     exit /b
 )
 
-rem Install setuptools if not already installed
-echo Checking for setuptools...
-python -m pip show setuptools >nul 2>&1
-if %errorlevel% neq 0 (
-    echo setuptools not found. Installing setuptools...
-    python -m pip install setuptools
-    if %errorlevel% neq 0 (
-        echo An error occurred during the installation of setuptools.
-        pause
-        exit /b
-    )
-) else (
-    echo setuptools is already installed.
-)
-
-rem Check if the required Python modules are installed
-echo Checking for required Python modules...
+rem Check if requirements.txt exists
 if not exist requirements.txt (
     echo requirements.txt file not found.
     pause
     exit /b
 )
 
-for /F "usebackq tokens=*" %%i in ("requirements.txt") do (
-    echo Checking for %%i...
-    python -m pip show %%i >nul 2>&1
-    if %errorlevel% neq 0 (
-        echo Installing %%i...
-        pip install %%i
-        if !errorlevel! neq 0 (
-            echo An error occurred during the installation of the dependency %%i.
-            pause
-            exit /b
-        ) else (
-            powershell -command "$Host.UI.RawUI.ForegroundColor = 'Green'; Write-Host 'Successfully installed the dependency %%i.'; $Host.UI.RawUI.ForegroundColor = 'White'"
-        )
-    ) else (
-        powershell -command "$Host.UI.RawUI.ForegroundColor = 'Green'; Write-Host '%%i is already installed.'; $Host.UI.RawUI.ForegroundColor = 'White'"
-    )
+rem Create and activate virtual environment if not already done
+if exist venv (
+    echo Virtual Environment Already Exists
+) else (
+    echo Creating Python Virtual Environment
+    py -m venv venv
 )
 
-echo All Python dependencies have been successfully installed.
+echo Sourcing Python Virtual Environment
+if exist "venv\Scripts\activate.bat" (
+    call .\venv\Scripts\activate.bat
+) else (
+    call .\venv\bin\activate
+)
+
+rem Install required Python modules
+echo Installing Modules
+py -m pip install -r requirements.txt
+
+rem Check if installation was successful
+if %errorlevel% neq 0 (
+    echo Failed to install required modules. Check the error messages above.
+    pause
+    exit /b
+)
 
 rem Launch the Python script
-python VRCST.py
+py VRCST.py
 
 rem Pause to display results (optional)
 pause
